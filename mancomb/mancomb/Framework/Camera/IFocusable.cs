4cde018ae308fea11e2dfa9419b2865d0eb6fe9f
TimeTourist <alla.bra.namn.slut@gmail.com>
2012-10-08 22:07:33 +0200

    Added Camera

---
+++ mancomb/mancomb/Framework/Camera/IFocusable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace mancomb.Framework.Camera
+{
+    /// <summary>
+    /// Taken from http://stackoverflow.com/questions/712296/xna-2d-camera-engine-that-follows-sprite
+    /// </summary>
+    public interface IFocusable
+    {
+        Vector2 Position { get; }
+    }
+
+}
